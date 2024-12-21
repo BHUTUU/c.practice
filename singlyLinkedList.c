2f720be7c915712173a19e98bc333b6b838322fa
BHUTUU <raj259942@gmail.com>
2024-12-22 02:45:05 +0530

    udpated delete options for linked list.

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -6,11 +6,14 @@ struct mychain
     int num;
     struct mychain* next;
 };
+
 void linkedListTraversal(struct mychain* head) {
+    printf("[ ");
     while(head != NULL){
-        printf("%d\n", head->num);
+        printf("%d ", head->num);
         head = head->next;
     }
+    printf("]\n");
 }
 struct mychain* insertAtHead(struct mychain* head, int data) {
     struct mychain* newNode = (struct mychain*) malloc(sizeof(struct mychain));
@@ -29,7 +32,7 @@ struct mychain* insertAtIndex(struct mychain* head, int data, int index) {
     newNode->num = data;
     newNode->next = ptr->next;
     ptr->next = newNode;
-    return newNode;
+    return head;
 }
 struct mychain* insertAtEnd(struct mychain* head, int data){
     struct mychain* newNode = (struct mychain*) malloc(sizeof(struct mychain));
@@ -40,7 +43,7 @@ struct mychain* insertAtEnd(struct mychain* head, int data){
     }
     ptr->next = newNode;
     newNode->next = NULL;
-    return newNode;
+    return head;
 }
 struct mychain* insertAfterNode(struct mychain* toPutAfterNode, int num) {
     struct mychain* newNode = (struct mychain*) malloc(sizeof(struct mychain));
@@ -48,6 +51,74 @@ struct mychain* insertAfterNode(struct mychain* toPutAfterNode, int num) {
     newNode->next = toPutAfterNode->next;
     toPutAfterNode->next = newNode;
     return newNode;
+}
+struct mychain* deleteHead(struct mychain* head) {
+    struct mychain* temp = head->next;
+    free(head);
+    return temp;
+}
+struct mychain* deleteAtIndex(struct mychain* head, int index) {
+    // struct mychain* temp = head;
+    // int chainIndex = 0;
+    // while(chainIndex!= index - 1) {
+    //     if(temp->next !=NULL) {
+    //         temp = temp->next;
+    //         chainIndex++;
+    //     } else {
+    //         printf("Index out of range\n");
+    //         return head;
+    //     }
+    // }
+    // struct mychain* toDeleteNode=temp->next;
+    // temp->next = toDeleteNode->next;
+    // free(toDeleteNode);
+    // return head;
+
+    //for more clarity it can be also done as follow:
+
+    struct mychain* p = head;
+    struct mychain* q = p->next;
+    for(int i=0; i<index-1; i++) {
+        if (q != NULL && q->next !=NULL) {
+            p=p->next;
+            q=q->next;
+        } else {
+            printf("Index out of range\n");
+            return head;
+        }
+    }
+    p->next = q->next;
+    free(q);
+    return head;
+}
+struct mychain* deleteLastNode(struct mychain* head) {
+    struct mychain* p = head;
+    struct mychain* q = p->next;
+    while(q->next!=NULL) {
+        p=p->next;
+        q=q->next;
+    }
+    free(q);
+    p->next = NULL;
+    return head;
+}
+struct mychain* deleteNodeWithNum(struct mychain* head, int numValue) {
+    struct mychain* p=head;
+    struct mychain* q=p->next;
+    if(p->num==numValue) {
+        head = q;
+        free(p);
+        return head;
+    }
+    while(q->num!=numValue && q->next!=NULL) {
+        p=p->next;
+        q=q->next;
+    }
+    if(q->num==numValue) {
+        p->next = q->next;
+        free(q);
+    }
+    return head;
 }
 int main(int argc, char *argv[]) {
     struct mychain* head = (struct mychain*) malloc(sizeof(struct mychain));
@@ -59,10 +130,24 @@ int main(int argc, char *argv[]) {
     second->next = third;
     third->num = 3;
     third->next = NULL;
-    struct mychain* newHead = insertAtHead(head, 56);
-    insertAtIndex(newHead, 4, 1);
-    insertAtEnd(newHead, 64);
-    struct mychain* current = newHead;
+    head = insertAtHead(head, 56);
+    printf("new head is %d\n", head->num);
+    head=insertAtIndex(head, 4, 1);
+    head=insertAtEnd(head, 64);
+    struct mychain* current = head;
+    printf("Printing chain elements before delete operation of head\n");
     linkedListTraversal(current);
+    printf("Printing chain elements after delete operation of head\n");
+    head=deleteHead(current);
+    linkedListTraversal(head);
+    printf("Printing chain elements after delete at index 2\n");
+    head=deleteAtIndex(head, 2);
+    linkedListTraversal(head);
+    printf("printing chain elements after deleting end element\n");
+    head=deleteLastNode(head);
+    linkedListTraversal(head);
+    printf("Printing chain elements after deleting node with num=1\n");
+    head=deleteNodeWithNum(head, 1);
+    linkedListTraversal(head);
     return 0;
 }
